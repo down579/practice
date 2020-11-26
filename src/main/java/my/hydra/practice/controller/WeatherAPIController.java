0e0aae71b3c925d0d7f4d6816b3c88da65c2c214
down579 <skyblue436@gmail.com>
2020-11-26 21:16:07 +0900

    날씨정보api 컨트롤러 추가

---
+++ src/main/java/my/hydra/practice/controller/WeatherAPIController.java
@@ -0,0 +1,9 @@
+package my.hydra.practice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherAPIController {
+}
