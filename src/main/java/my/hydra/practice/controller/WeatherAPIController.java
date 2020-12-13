856f6cb0f76ab567d4cc9378bc6124be7cf53996
down579 <skyblue436@gmail.com>
2020-12-14 06:47:05 +0900

    날씨정보api 동네예보조회, 초단기예보조회 추가

--- src/main/java/my/hydra/practice/controller/WeatherAPIController.java
+++ src/main/java/my/hydra/practice/controller/WeatherAPIController.java
@@ -16,12 +16,76 @@ public class WeatherAPIController {
     private final String baseApiUrl = "http://apis.data.go.kr/1360000";
     private final String apiKey = "8NSiUwnrP48P0h9w%2F2SCHeaoHqu4cf5nw28kgmOjDDa73GKAKOZrz%2BPkFX84gVmCmbjH%2BhjzdgXPcoqCtLj4bQ%3D%3D";
 
+    /**
+     * 날씨 초단기실황조회
+     * @param numOfRows
+     * @param pageNo
+     * @param baseDate
+     * @param baseTime
+     * @param nx
+     * @param ny
+     * @return
+     */
     @RequestMapping(value ="/getUltraSrtNcst", method = RequestMethod.GET)
     public String getUltraSrtNcst(int numOfRows, int pageNo, String baseDate, String baseTime,
                                   int nx, int ny) {
         String result = "";
         try {
             String url = baseApiUrl + "/VilageFcstInfoService/getUltraSrtNcst?serviceKey="
+                    + apiKey + "&numOfRows=" + numOfRows + "&pageNo=" + pageNo
+                    + "&base_date=" + baseDate + "&base_time=" + baseTime + "&nx=" + nx
+                    + "&ny=" + ny + "&dataType=JSON";
+            result = requestWeather(url);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 날씨 초단기예보조
+     * @param numOfRows
+     * @param pageNo
+     * @param baseDate
+     * @param baseTime
+     * @param nx
+     * @param ny
+     * @return
+     */
+    @RequestMapping(value ="/getUltraSrtFcst", method = RequestMethod.GET)
+    public String getUltraSrtFcst(int numOfRows, int pageNo, String baseDate, String baseTime,
+                                  int nx, int ny) {
+        String result = "";
+        try {
+            String url = baseApiUrl + "/VilageFcstInfoService/getUltraSrtFcst?serviceKey="
+                    + apiKey + "&numOfRows=" + numOfRows + "&pageNo=" + pageNo
+                    + "&base_date=" + baseDate + "&base_time=" + baseTime + "&nx=" + nx
+                    + "&ny=" + ny + "&dataType=JSON";
+            result = requestWeather(url);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 날씨 동네예보조회
+     * @param numOfRows
+     * @param pageNo
+     * @param baseDate
+     * @param baseTime
+     * @param nx
+     * @param ny
+     * @return
+     */
+    @RequestMapping(value ="/getVilageFcst", method = RequestMethod.GET)
+    public String getVilageFcst(int numOfRows, int pageNo, String baseDate, String baseTime,
+                                  int nx, int ny) {
+        String result = "";
+        try {
+            String url = baseApiUrl + "/VilageFcstInfoService/getVilageFcst?serviceKey="
                     + apiKey + "&numOfRows=" + numOfRows + "&pageNo=" + pageNo
                     + "&base_date=" + baseDate + "&base_time=" + baseTime + "&nx=" + nx
                     + "&ny=" + ny + "&dataType=JSON";
