e5d5a02bbeb527b4a43bbc592f67b9f90d1129c0
down579 <skyblue436@gmail.com>
2021-03-15 20:54:56 +0900

    네이버 뉴스 검색 API 조회 페이지 구현 중 : 페이징 진행 필요

--- src/main/java/my/hydra/practice/controller/NaverApiViewController.java
+++ src/main/java/my/hydra/practice/controller/NaverApiViewController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class TestController {
+public class NaverApiViewController {
 
     @RequestMapping(method= RequestMethod.GET, value="/template")
     public String template() {
         return "practice-template";
     }
-    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    @RequestMapping(method = RequestMethod.GET, value = "/naver/news")
     public String test() {
-        return "test";
+        return "navernews";
     }
 }
